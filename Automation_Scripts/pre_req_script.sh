d39e34e499dce2eeb3c7146165abefbd4a3a4c36
Fellware <patrickkappen@gmail.com>
2024-01-16 16:39:26 +0100

    Changed ip settings

--- Automation_Scripts/pre_req_script.sh
+++ Automation_Scripts/pre_req_script.sh
@@ -3,5 +3,8 @@
 # This script will do the following things:
 # Complete the installation of the laptop that will start every VM
 
-sudo nmcli con modify 'HomeSweetHome' ifname wlo1 ipv4.method manual ipv4.addresses 192.168.1.150/24 gw4 192.168.1.1
-sudo systemctl restart NetworkManager.service
+sudo nmcli con mod enp46s0 ipv4.addresses 192.168.1.150/24
+sudo nmcli con mod enp46s0 ipv4.gateway 192.168.1.1
+sudo nmcli con mod enp46s0 ipv4.method manual
+sudo nmcli con mod enp46s0 ipv4.dns "192.168.1.1"
+sudo nmcli con up enp46s0
