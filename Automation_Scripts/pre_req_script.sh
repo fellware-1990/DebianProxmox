8da990aa144cabf3df585992f17c41d548e046f5
Fellware <patrickkappen@gmail.com>
2024-01-16 15:59:15 +0100

    Setup IP adress

--- Automation_Scripts/pre_req_script.sh
+++ Automation_Scripts/pre_req_script.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
 # This script will do the following things:
 # Complete the installation of the laptop that will start every VM
+
+sudo nmcli con modify 'HomeSweetHome' ifname wlo1 ipv4.method manual ipv4.addresses 192.168.1.150/24 gw4 192.168.1.1
+sudo systemctl restart NetworkManager.service
