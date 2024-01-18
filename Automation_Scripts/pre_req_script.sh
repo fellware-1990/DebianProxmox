6e08939b06536775fc82a73131434a4514387b19
Fellware <patrickkappen@gmail.com>
2024-01-18 12:55:15 +0100

    ifelse statement

--- Automation_Scripts/pre_req_script.sh
+++ Automation_Scripts/pre_req_script.sh
@@ -7,16 +7,16 @@ ethcon=$(nmcli -t -f NAME c show --active | grep -i "wired")
 hostip=$(hostname -I)
 
 function setupip {
-    if (( $hostip == '192.168.1.150' ))
+    if (( "$hostip" == '192.168.1.150' ))
     then
         echo "Host Ip Address is already "$hostip""
     else
-    
+
             sudo nmcli connection modify "$ethcon" ipv4.address 192.168.1.150/24
             sudo nmcli connection modify "$ethcon" ipv4.gateway 192.168.1.1
             sudo nmcli connection modify "$ethcon" ipv4.method manual
             sudo nmcli connection modify "$ethcon" ipv4.dns '192.168.1.1'
-    
+
             sudo nmcli connection down "$ethcon"
             sudo nmcli connection up "$ethcon"
     fi
