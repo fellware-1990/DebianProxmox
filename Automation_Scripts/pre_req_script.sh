aec47619ca98a05b0ae2fe4213fa19854792f52b
Fellware <patrickkappen@gmail.com>
2024-01-18 12:16:29 +0100

    test with variable

--- Automation_Scripts/pre_req_script.sh
+++ Automation_Scripts/pre_req_script.sh
@@ -5,13 +5,13 @@
 
 ethcon=$(nmcli -t -f NAME c show --active | grep -i "wired")
 
-sudo nmcli connection modify '${ethcon}' ipv4.address 192.168.1.150/24
-sudo nmcli connection modify '${ethcon}' ipv4.gateway 192.168.1.1
-sudo nmcli connection modify '${ethcon}' ipv4.method manual
-sudo nmcli connection modify '${ethcon}' ipv4.dns '192.168.1.1'
+sudo nmcli connection modify '"$ethcon"' ipv4.address 192.168.1.150/24
+sudo nmcli connection modify '"$ethcon"' ipv4.gateway 192.168.1.1
+sudo nmcli connection modify '"$ethcon"' ipv4.method manual
+sudo nmcli connection modify '"$ethcon"' ipv4.dns '192.168.1.1'
 
-sudo nmcli connection down '${ethcon}'
-sudo nmcli connection up '${ethcon}'
+sudo nmcli connection down '"$ethcon"'
+sudo nmcli connection up '"$ethcon"'
 
 # eth_con=$(nmcli -t -f NAME c show --active | grep -i "wired")
 
