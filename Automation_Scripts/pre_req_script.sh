73d8e2e97a81815863e44e19417e36221a3a3040
Fellware <patrickkappen@gmail.com>
2024-01-18 12:03:09 +0100

    first test for script

--- Automation_Scripts/pre_req_script.sh
+++ Automation_Scripts/pre_req_script.sh
@@ -3,8 +3,17 @@
 # This script will do the following things:
 # Complete the installation of the laptop that will start every VM
 
-sudo nmcli con mod enp46s0 ipv4.addresses 192.168.1.150/24
-sudo nmcli con mod enp46s0 ipv4.gateway 192.168.1.1
-sudo nmcli con mod enp46s0 ipv4.method manual
-sudo nmcli con mod enp46s0 ipv4.dns "192.168.1.1"
-sudo nmcli con up enp46s0
+sudo nmcli connection modify 'Wired connection 1' ipv4.address 192.168.1.150/24
+sudo nmcli connection modify 'Wired connection 1' ipv4.gateway 192.168.1.1
+sudo nmcli connection modify 'Wired connection 1' ipv4.method manual
+sudo nmcli connection modify 'Wired connection 1' ipv4.dns '192.168.1.1'
+
+$ sudo nmcli connection down 'Wired connection 1'
+$ sudo nmcli connection up 'Wired connection 1'
+
+# eth_con=$(nmcli -t -f NAME c show --active | grep -i "wired")
+
+
+# Function for setting IP with If else to check IP 
+# Function should contain variables for IP adress
+# When function is called it should have the variables
