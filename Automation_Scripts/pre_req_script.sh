42faf75b799d7e4ca173e78ac0784a8731bc3d16
Fellware <patrickkappen@gmail.com>
2024-01-18 12:10:55 +0100

    test with variable

--- Automation_Scripts/pre_req_script.sh
+++ Automation_Scripts/pre_req_script.sh
@@ -3,13 +3,15 @@
 # This script will do the following things:
 # Complete the installation of the laptop that will start every VM
 
-sudo nmcli connection modify 'Wired connection 1' ipv4.address 192.168.1.150/24
-sudo nmcli connection modify 'Wired connection 1' ipv4.gateway 192.168.1.1
-sudo nmcli connection modify 'Wired connection 1' ipv4.method manual
-sudo nmcli connection modify 'Wired connection 1' ipv4.dns '192.168.1.1'
+eth_con=$(nmcli -t -f NAME c show --active | grep -i "wired")
 
-sudo nmcli connection down 'Wired connection 1'
-sudo nmcli connection up 'Wired connection 1'
+sudo nmcli connection modify '$eth_con' ipv4.address 192.168.1.150/24
+sudo nmcli connection modify '$eth_con' ipv4.gateway 192.168.1.1
+sudo nmcli connection modify '$eth_con' ipv4.method manual
+sudo nmcli connection modify '$eth_con' ipv4.dns '192.168.1.1'
+
+sudo nmcli connection down '$eth_con'
+sudo nmcli connection up '$eth_con'
 
 # eth_con=$(nmcli -t -f NAME c show --active | grep -i "wired")
 
